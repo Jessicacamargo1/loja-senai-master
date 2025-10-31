9ad97ff45b726694b3e8125c65e534d1ffab6e5c
jessicacamargo1 <jessicacamargo302009@gmail.com>
2025-10-31 20:52:31 -0300

    adiciona id e email ao customer

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { CustomersService } from "./customers.service";
 import { UpsertDTO } from "./dto/upsert.dto"
 // @ -> é um decorator
@@ -20,5 +20,12 @@ export class CustomersController {
     @Post()
     create(@Body() bodyCustomer: UpsertDTO) {
         return this.customersService.create(bodyCustomer);
+    
+    }
+
+    @Delete(':id')
+    delete(@Param('id') customer_id: number) {
+        return customer_id;
+
     }
 }
